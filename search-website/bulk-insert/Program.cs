8ca5bfb9ba54f37583318c8b6289d5b037e2aecb
SayCloud <59239741+SayCloud@users.noreply.github.com>
2022-05-23 14:50:24 +0800

    Update Program.cs

--- search-website/bulk-insert/Program.cs
+++ search-website/bulk-insert/Program.cs
@@ -14,8 +14,8 @@ namespace ConsoleApp1
     class Program
     {
         const string BOOKS_URL = "https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/master/books.csv";
-        const string SEARCH_ENDPOINT = "https://YOUR-SEARCH-RESOURCE-NAME.search.windows.net";
-        const string SEARCH_KEY = "YOUR-SEARCH-ADMIN-KEY";
+        const string SEARCH_ENDPOINT = "https://jucqsearch2022.search.windows.net";
+        const string SEARCH_KEY = "vWZOeW9UgUStruhoAJNlF08gBRNFLgy8t92hoKynOGAzSeAf062A";
         const string SEARCH_INDEX_NAME = "good-books";
 
         static void Main(string[] args)
